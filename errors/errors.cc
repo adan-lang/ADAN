38dba62604e559c8c868c43ec6a9b3bfbf8bf9a1
lily ❤ <lily@transgirls.love>
2026-07-13 00:37:56 -0700

    upd: ignore hideous styles; i have self-diagnosed ocd i swear

--- errors/errors.cc
+++ errors/errors.cc
@@ -6,21 +6,72 @@
 void report(
     Error& error
 ) {
-    // if (error.severity == SEVERITY_ERROR)
-    // {
-    //     std::cout << "\x1b[1;31m[Error]\x1b[0m: " << error.message << " on line " << error.line_num << ":" << error.col_num << "\n\n";
-    // }
-
-    // else if (error.severity == SEVERITY_WARN)
-    // {
-    //     std::cout << "\x1b[1;33m[Warning]\x1b[0m: " << error.message << " on line " << error.line_num << ":" << error.col_num << "\n\n";
-    // }
-
     const char* level_color = (error.severity == SEVERITY_ERROR) ? "\x1b[1;31m" : "\x1b[1;33m";
     const char* level_word = (error.severity == SEVERITY_ERROR) ? "error" : "warning";
 
     std::string line_num = std::to_string(error.line_num);
     std::string pad(line_num.size(), ' ');
 
-    // @todo header, location, gutter + source, caret span, hint
+    std::cout <<
+
+        level_color << level_word << "[" << error.code << "]" << "\x1b[0m" << "\x1b[1m" << ": " << error.message << "\x1b[0m"
+        
+    << "\n";
+
+    std::cout <<
+    
+        pad << "\x1b[1;36m" << " --> " << "\x1b[0m" << error.line_num << ":" << error.col_start
+    
+    << "\n";
+
+    std::cout <<
+
+        pad << " " << "|" << "\x1b[0m"
+
+    << "\n";
+
+    std::cout <<
+
+        "\x1b[1;36m" << "\x1b[1;36m" << line_num << " | " << "\x1b[0m" << error.line
+
+    << "\n";
+
+    std::cout << pad << " " << "\x1b[1;36m" << "|" << "\x1b[0m" << " ";
+
+    for (int i{1}; i < error.col_start; ++i)
+    {
+        std::cout << ' ';
+    }
+
+    std::cout << level_color;
+
+    int span = std::max(1, error.col_end - error.col_start);
+
+    for (int i{}; i < span; ++i)
+    {
+        std::cout << '^';
+    }
+
+    std::cout <<
+    
+        "\x1b[0m"
+        
+    << "\n";
+
+    if (error.hint.has_value()) // exists
+    {
+        std::cout <<
+
+            pad << " " << "\x1b[1;36m" << "|" << "\x1b[0m"
+
+        << "\n";
+
+        std::cout <<
+
+            "\x1b[1;32m" << "help" << "\x1b[0m" << ": " << *error.hint
+
+        << "\n";
+    }
+
+    std::cout << "\n";
 }
