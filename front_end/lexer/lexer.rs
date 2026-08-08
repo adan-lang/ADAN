7d7eb5bd10ed991e7864301a8a39b4657e0453bc
lily <lily@transgirls.love>
2026-08-08 11:21:23 -0700

    fix: return char instead of byte array

--- front_end/lexer/lexer.rs
+++ front_end/lexer/lexer.rs
@@ -6,15 +6,20 @@ pub struct Lexer {
 }
 
 impl Lexer {
-    fn peek(&self, offset: usize) -> Option<u8> {
-        self.source.as_bytes().get(self.position + offset).copied()
+    fn peek(&self, offset: usize) -> char {
+        self.source
+            .chars()
+            .nth(self.position + offset)
+            .unwrap_or('\0')
     }
 
-    fn peek_next(&self) -> Option<u8> {
+    fn peek_next(&self) -> char {
         self.peek(1)
     }
 
-    fn consume(&self) {}
+    fn consume(&mut self) {
+        self.position += 1;
+    }
 
     pub fn lex(&self) -> Vec<Token> {
         let tokens: Vec<Token> = vec![];
