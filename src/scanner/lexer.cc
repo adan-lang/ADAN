622f3a084e2c19f1ee736e8bb305c076d6f05055
lily <lily@transgirls.love>
2026-09-24 23:27:24 -0700

    get the lexer working so far!

--- src/scanner/lexer.cc
+++ src/scanner/lexer.cc
@@ -1,8 +1,214 @@
-#include <string>
-
 #include "lexer.hh"
 
-Lexer::Lexer(std::string source_code)
-    : source_code(source_code), pos(0) {}
+Token Lexer::next()
+{
+    skip_spaces();
+    skip_comments();
 
-Lexer::~Lexer() {}
+    switch (const auto curr{peek()}; curr)
+    {
+    case '+':
+        advance();
+        if (peek() == '+')
+        {
+            advance();
+            return Token{TokenKind::Increment, "++"};
+        }
+        if (peek() == '=')
+        {
+            advance();
+            return Token{TokenKind::PlusAssign, "+="};
+        }
+        return Token{TokenKind::Plus, "+"};
+
+    case '-':
+        advance();
+        if (peek() == '-')
+        {
+            advance();
+            return Token{TokenKind::Decrement, "--"};
+        }
+        if (peek() == '=')
+        {
+            advance();
+            return Token{TokenKind::MinusAssign, "-="};
+        }
+        return Token{TokenKind::Minus, "-"};
+
+    case '*':
+        advance();
+        if (peek() == '=')
+        {
+            advance();
+            return Token{TokenKind::MultiplyAssign, "*="};
+        }
+        return Token{TokenKind::Multiply, "*"};
+
+    case '/':
+        advance();
+        if (peek() == '=')
+        {
+            advance();
+            return Token{TokenKind::DivideAssign, "/="};
+        }
+        return Token{TokenKind::Divide, "/"};
+
+    case '%':
+        advance();
+        if (peek() == '=')
+        {
+            advance();
+            return Token{TokenKind::ModuloAssign, "%="};
+        }
+        return Token{TokenKind::Modulo, "%"};
+
+    case '>':
+        advance();
+        if (peek() == '=')
+        {
+            advance();
+            return Token{TokenKind::GreaterEqual, ">="};
+        }
+        return Token{TokenKind::GreaterThan, ">"};
+
+    case '<':
+        advance();
+        if (peek() == '=')
+        {
+            advance();
+            return Token{TokenKind::LessEqual, "<="};
+        }
+        if (peek() == '-')
+        {
+            advance();
+            return Token{TokenKind::BackArrow, "<-"};
+        }
+        return Token{TokenKind::LessThan, "<"};
+
+    case '=':
+        advance();
+        if (peek() == '=')
+        {
+            advance();
+            return Token{TokenKind::EqualEqual, "=="};
+        }
+        return Token{TokenKind::Equal, "="};
+
+    case ';':
+        advance();
+        return Token{TokenKind::SemiColon, ";"};
+
+    case '#':
+        advance();
+        return Token{TokenKind::Hashtag, "#"};
+
+    case '$':
+        advance();
+        if (peek() == '{')
+        {
+            advance();
+            return Token{TokenKind::Interpolate, "${"};
+        }
+        return Token{TokenKind::Dollar, "$"};
+
+    case '(':
+        advance();
+        return Token{TokenKind::LParen, "("};
+
+    case ')':
+        advance();
+        return Token{TokenKind::RParen, ")"};
+
+    case '[':
+        advance();
+        return Token{TokenKind::LBracket, "["};
+
+    case ']':
+        advance();
+        return Token{TokenKind::RBracket, "]"};
+
+    case '{':
+        advance();
+        return Token{TokenKind::LBrace, "{"};
+
+    case '}':
+        advance();
+        return Token{TokenKind::RBrace, "}"};
+
+    case '"':
+        advance();
+        return Token{TokenKind::Quote, "\""};
+
+    case '\'':
+        advance();
+        return Token{TokenKind::Apostrophe, "'"};
+
+    case '.':
+        if (peek(1) == '.' && peek(2) == '.')
+        {
+            advance();
+            advance();
+            advance();
+            return Token{TokenKind::Ellipsis, "..."};
+        }
+        break;
+
+    case '`':
+        advance();
+        return Token{TokenKind::Tick, "`"};
+
+    case ',':
+        advance();
+        return Token{TokenKind::Comma, ","};
+
+    default:
+        if (std::isdigit(curr))
+        {
+            const auto start{position};
+            bool is_float{};
+
+            while (!is_eof())
+            {
+                if (std::isdigit(source[position]))
+                {
+                    advance();
+                }
+                else if (source[position] == '.' &&
+                         !is_float && position + 1 < source.length() &&
+                         std::isdigit(source[position + 1]))
+                {
+                    is_float = true;
+                    advance();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            const std::string value{source.substr(start, position - start)};
+            return Token{is_float ? TokenKind::Float : TokenKind::Int, value};
+        }
+
+        for (const auto &[key, value] : keywords)
+        {
+            if (value == source.substr(position, value.length()) &&
+                !isalnum(source[position + value.length()]))
+            {
+                position += value.length();
+                return Token{key, std::string(value)};
+            }
+        }
+
+        if (std::isalnum(curr) || curr == '_')
+        {
+            const auto start{position};
+            while (!is_eof() && std::isalnum(source[position]) || source[position] == '_')
+                advance();
+
+            return Token{TokenKind::Ident, std::string(source.substr(start, position - start))};
+        }
+
+        return Token{TokenKind::Eof, ""};
+    }
+}
--- src/scanner/lexer.hh
+++ src/scanner/lexer.hh
@@ -1,295 +1,87 @@
 #pragma once
 
-#include <string>
-#include <vector>
-#include <cctype>
-
 #include "token.hh"
+
+#include <cctype>
+#include <unordered_map>
+
+const std::unordered_map<TokenKind, std::string_view> keywords = {
+    {TokenKind::Local, "local"},
+    {TokenKind::Function, "function"},
+    {TokenKind::If, "if"},
+    {TokenKind::While, "while"},
+    {TokenKind::For, "for"},
+    {TokenKind::In, "in"},
+};
 
 class Lexer
 {
 public:
-    std::vector<Token> lex()
-    {
-        std::vector<Token> tokens;
+    explicit Lexer(std::string_view source) : source{source} {}
 
-        while (!eof())
-        {
-            skip_ws();
-            char curr = current();
-
-            switch (curr)
-            {
-            case '+':
-                tokens.emplace_back(Token{
-                    TokenKind::ADD,
-                    "+",
-                    line,
-                    column,
-                });
-                consume();
-                break;
-
-            case '-':
-                tokens.emplace_back(Token{
-                    TokenKind::SUBTRACT,
-                    "-",
-                    line,
-                    column,
-                });
-                consume();
-                break;
-
-            case '*':
-                tokens.emplace_back(Token{
-                    TokenKind::MULTIPLY,
-                    "*",
-                    line,
-                    column,
-                });
-                consume();
-                break;
-
-            case '/':
-                tokens.emplace_back(Token{
-                    TokenKind::DIVIDE,
-                    "/",
-                    line,
-                    column,
-                });
-                consume();
-                break;
-
-            case '%':
-                tokens.emplace_back(Token{
-                    TokenKind::MODULO,
-                    "%",
-                    line,
-                    column,
-                });
-                consume();
-                break;
-
-            case '=':
-                tokens.emplace_back(Token{
-                    TokenKind::EQUALS,
-                    "=",
-                    line,
-                    column,
-                });
-                consume();
-                break;
-
-            case '@':
-                tokens.emplace_back(Token{
-                    TokenKind::AT,
-                    "@",
-                    line,
-                    column,
-                });
-                consume();
-                break;
-
-            case '.':
-                tokens.emplace_back(Token{
-                    TokenKind::PERIOD,
-                    ".",
-                    line,
-                    column,
-                });
-                consume();
-                break;
-
-            case '(':
-                tokens.emplace_back(Token{
-                    TokenKind::LPAREN,
-                    "(",
-                    line,
-                    column,
-                });
-                consume();
-                break;
-
-            case ')':
-                tokens.emplace_back(Token{
-                    TokenKind::RPAREN,
-                    ")",
-                    line,
-                    column,
-                });
-                consume();
-                break;
-
-            case '"':
-                tokens.emplace_back(lex_string());
-                break;
-
-            default:
-                if (isdigit(curr))
-                    tokens.emplace_back(lex_number());
-                else if (isalpha(curr) || curr == '_')
-                    tokens.emplace_back(lex_identifier());
-                else
-                    consume(); // skip unknown char for now
-                break;
-            }
-        }
-
-        return tokens;
-    }
-
-    Lexer(std::string source_file);
-
-    ~Lexer();
+    Token next();
 
 private:
-    std::string source_code;
-    int pos{};
-    int line{};
-    int column{};
+    std::string_view source{};
+    std::size_t position{};
 
-    /**
-     *
-     * @brief Returns the current character.
-     */
-    char current()
+    void advance()
     {
-        return source_code[pos];
+        if (!is_eof())
+            position++;
     }
 
-    /**
-     *
-     * @brief Peak *n* characters ahead, or 1 if unsupplied.
-     */
-    char peek(int offset = 1)
+    char peek(int offset = 0) const
     {
-        return source_code[pos + offset];
+        return is_eof() ? '\0' : source[position + offset];
     }
 
-    /**
-     *
-     * @brief Return the current position, then advance by 1.
-     */
-    char consume()
+    void skip_spaces()
     {
-        if (!eof())
+        while (!is_eof() && std::isspace(peek()))
+            advance();
+    }
+
+    void skip_comments()
+    {
+        while (!is_eof())
         {
-            char curr = source_code[pos++];
+            skip_spaces();
 
-            if (curr == '\n')
+            if (peek() == ';' && peek(1) == '-' && peek(2) == '-')
             {
-                line++;
-                column = 0;
+                advance();
+                advance();
+                advance();
+
+                while (!is_eof())
+                {
+                    if (peek() == '-' && peek(1) == '-' && peek(2) == ';')
+                    {
+                        advance();
+                        advance();
+                        advance();
+
+                        break;
+                    }
+
+                    advance();
+                }
+            }
+            else if (peek() == ';')
+            {
+                while (!is_eof() && peek() != '\n')
+                    advance();
             }
             else
             {
-                column++;
-            }
-
-            return curr;
-        }
-
-        return '\0';
-    }
-
-    /**
-     *
-     * @brief Returns a boolean state depending on if it has reached
-     *        the end of the input stream.
-     */
-    bool eof()
-    {
-        return pos >= (int)source_code.length();
-    }
-
-    /**
-     *
-     * @brief Skip all whitespace-identified characters until the next
-     *        non-whitespace character.
-     */
-    void skip_ws()
-    {
-        while (!eof() && isspace(current()))
-        {
-            consume();
-        }
-    }
-
-    // private tokenizing stuff
-
-    Token lex_number()
-    {
-        int start_line = line;
-        int start_column = column;
-        std::string value;
-
-        while (!eof() && (isdigit(current()) || current() == '.'))
-        {
-            value += consume();
-        }
-
-        return Token{
-            TokenKind::NUMBER,
-            value,
-            start_line,
-            start_column,
-        };
-    }
-
-    Token lex_string()
-    {
-        int start_line = line;
-        int start_column = column;
-        std::string value;
-
-        consume();
-
-        while (!eof() && current() != '"')
-        {
-            if (current() == '\\' && peek() == '"') // escaped quote \"
-            {
-                consume();
-                value += consume();
-            }
-            else
-            {
-                value += consume();
+                break;
             }
         }
-
-        if (eof())
-        {
-            // unterminated string, do error stuff later lol
-        }
-        else
-        {
-            consume();
-        }
-
-        return Token{
-            TokenKind::STRING,
-            value,
-            start_line,
-            start_column,
-        };
     }
 
-    Token lex_identifier()
+    bool is_eof() const
     {
-        int start_line = line;
-        int start_column = column;
-        std::string value;
-
-        while (!eof() && (isalnum(current()) || current() == '_'))
-        {
-            value += consume();
-        }
-
-        return Token{
-            TokenKind::IDENT,
-            value,
-            start_line,
-            start_column,
-        };
+        return position >= source.length();
     }
 };
